0aae84aa1a2a6707e4a37f8282b109b44e354c37
syee <stevenyee64@gmail.com>
2019-12-01 14:26:30 -0800

    Refactoring variables names

--- yeeStevenHW3.py
+++ yeeStevenHW3.py
@@ -5,19 +5,16 @@ Date: 12/01/2019
 Description: Python assignment for ECON 280
 """
 
-import random
 import numpy
 from numpy import genfromtxt
-import quantecon
 import pandas
 from scipy import optimize
-import matplotlib
 
 #Part 1: Median Regression
 
 sampleSize = 200
 replications = 1000
-estimates = numpy.zeros(replications)
+dgBetaEstimates = numpy.zeros(replications)
 
 
 # Gradient-free optimization method
@@ -35,12 +32,12 @@ for count in range(replications):
     lamda = 1 + numpy.random.poisson(5, sampleSize)
     X = 1./(lamda + 1)
     Y = numpy.random.exponential(X, sampleSize)
-    estimates[count] = estimateBeta(X, Y, sampleSize)
+    dgBetaEstimates[count] = estimateBeta(X, Y, sampleSize)
 
 def estimateEmpiricalBeta(X, Y, numObservations):
     return estimateBeta(X, Y, numObservations)
 
-# Is there a better way to read in the data?
+# Is there a better way to read in the data with numpy?
 empiricalData = genfromtxt('HW3_data.csv', delimiter=',')
 cleanedData = numpy.delete(empiricalData, 0, axis=0)
 numObservations = len(cleanedData)
@@ -50,7 +47,7 @@ for count in range(numObservations):
     empiricalX[count] = cleanedData[count][0]
     empiricalY[count] = cleanedData[count][1]
 
-empiricalEstimate = estimateEmpiricalBeta(empiricalX, empiricalY, numObservations)
+empiricalBetaEstimate = estimateEmpiricalBeta(empiricalX, empiricalY, numObservations)
 
-print("The data generated beta should be approximately 0.6931. This program generated a beta of " + str(numpy.mean(estimates)) + ".")
-print("The empirical data should have a beta of approximately 1.3141. This program estimates the beta to be " + str(empiricalEstimate[0]) + ".")
+print("The data generated beta should be approximately 0.6931. This program generated a beta of " + str(numpy.mean(dgBetaEstimates)) + ".")
+print("The empirical data should have a beta of approximately 1.3141. This program estimates the beta to be " + str(empiricalBetaEstimate[0]) + ".")
