681296078fc4af3b7dc365a97373596e434e8568
syee <stevenyee64@gmail.com>
2019-12-01 14:21:51 -0800

    Part 1 Finished

--- yeeStevenHW3.py
+++ yeeStevenHW3.py
@@ -7,6 +7,7 @@ Description: Python assignment for ECON 280
 
 import random
 import numpy
+from numpy import genfromtxt
 import quantecon
 import pandas
 from scipy import optimize
@@ -36,10 +37,20 @@ for count in range(replications):
     Y = numpy.random.exponential(X, sampleSize)
     estimates[count] = estimateBeta(X, Y, sampleSize)
 
-print(numpy.mean(estimates))
+def estimateEmpiricalBeta(X, Y, numObservations):
+    return estimateBeta(X, Y, numObservations)
 
+# Is there a better way to read in the data?
+empiricalData = genfromtxt('HW3_data.csv', delimiter=',')
+cleanedData = numpy.delete(empiricalData, 0, axis=0)
+numObservations = len(cleanedData)
+empiricalX = numpy.zeros(numObservations)
+empiricalY = numpy.zeros(numObservations)
+for count in range(numObservations):
+    empiricalX[count] = cleanedData[count][0]
+    empiricalY[count] = cleanedData[count][1]
 
+empiricalEstimate = estimateEmpiricalBeta(empiricalX, empiricalY, numObservations)
 
-
-
-
+print("The data generated beta should be approximately 0.6931. This program generated a beta of " + str(numpy.mean(estimates)) + ".")
+print("The empirical data should have a beta of approximately 1.3141. This program estimates the beta to be " + str(empiricalEstimate[0]) + ".")
