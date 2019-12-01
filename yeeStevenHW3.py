64ff5065ec8e2360f3599abb14eeed9edac65c5f
syee <stevenyee64@gmail.com>
2019-12-01 13:48:26 -0800

    Estimating beta correctly without input

--- yeeStevenHW3.py
+++ yeeStevenHW3.py
@@ -1,7 +1,45 @@
 # -*- coding: utf-8 -*-
 """
-Spyder Editor
-
-This is a temporary script file.
+Author: Steven Yee
+Date: 12/01/2019
+Description: Python assignment for ECON 280
 """
+
+import random
+import numpy
+import quantecon
+import pandas
+from scipy import optimize
+import matplotlib
+
+#Part 1: Median Regression
+
+sampleSize = 200
+replications = 1000
+estimates = numpy.zeros(replications)
+
+
+# Gradient-free optimization method
+def optimizationMethod(b, X, Y, length):
+    value = (abs(Y - X*b)/length).sum()
+    return value
+
+
+def estimateBeta(X, Y, length):
+    estimate = optimize.fmin(optimizationMethod, [1], disp=False, args=(X, Y, length,))
+    return estimate
+
+
+for count in range(replications):
+    lamda = 1 + numpy.random.poisson(5, sampleSize)
+    X = 1./(lamda + 1)
+    Y = numpy.random.exponential(X, sampleSize)
+    estimates[count] = estimateBeta(X, Y, sampleSize)
+
+print(numpy.mean(estimates))
+
+
+
+
+
 
